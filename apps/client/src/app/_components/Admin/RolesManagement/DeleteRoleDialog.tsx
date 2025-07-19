19c63907f7e1af57ca9c97a6f2682150ddcba32a
nazariiko <nazarii.dev@gmail.com>
2025-07-19 11:06:02 +0300

    improve submit button, add to role deleting proccess

--- apps/client/src/app/_components/Admin/RolesManagement/DeleteRoleDialog.tsx
+++ apps/client/src/app/_components/Admin/RolesManagement/DeleteRoleDialog.tsx
@@ -9,9 +9,15 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
+import { Form } from "@/components/ui/form";
 import { deleteUserRole } from "@/lib/api/admin/user-roles";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { IGetUserRolesWithIsEditableResponse } from "@repo/dto";
+import { useForm } from "react-hook-form";
 import { toast } from "sonner";
+import { z } from "zod";
+
+const formSchema = z.object({});
 
 export function DeleteRoleDialog({
   isOpen,
@@ -24,8 +30,12 @@ export function DeleteRoleDialog({
   onDelete: (id: number) => void;
   role: IGetUserRolesWithIsEditableResponse;
 }) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {},
+  });
+
   const handleDelete = async () => {
-    // todo: show preloader or use form with submit button
     const { ok, message } = await deleteUserRole(role.id);
     if (ok) {
       toast.success(message);
@@ -42,25 +52,32 @@ export function DeleteRoleDialog({
   return (
     <AlertDialog open={isOpen} onOpenChange={onClose}>
       <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>
-            Ви впевнені, що хочете видалити роль <br />
-            <span className="font-bold">"{role.uaName}"</span>?
-          </AlertDialogTitle>
-          <AlertDialogDescription>
-            Всі користувачі, які мають цю роль, втратять її та всі права, які
-            належать цій ролі.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Скасувати</AlertDialogCancel>
-          <AlertDialogAction
-            onClick={() => handleDelete()}
-            className="bg-red-600 text-white hover:bg-red-700 dark:bg-red-700 dark:hover:bg-red-800"
-          >
-            Продовжити
-          </AlertDialogAction>
-        </AlertDialogFooter>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(handleDelete)}>
+            <AlertDialogHeader>
+              <AlertDialogTitle>
+                Ви впевнені, що хочете видалити роль <br />
+                <span className="font-bold">"{role.uaName}"</span>?
+              </AlertDialogTitle>
+              <AlertDialogDescription>
+                Всі користувачі, які мають цю роль, втратять її та всі права,
+                які належать цій ролі.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Скасувати</AlertDialogCancel>
+              <SubmitButton
+                className="w-[150px]"
+                variant={"destructive"}
+                size={"default"}
+                loadingText="Видалення..."
+                isSubmitting={form.formState.isSubmitting}
+              >
+                Видалити
+              </SubmitButton>
+            </AlertDialogFooter>
+          </form>
+        </Form>
       </AlertDialogContent>
     </AlertDialog>
   );
--- apps/client/src/app/_components/ui/SubmitButton.tsx
+++ apps/client/src/app/_components/ui/SubmitButton.tsx
@@ -1,23 +1,50 @@
 import { Button } from "@/components/ui/button";
 import { Spinner } from "@/components/ui/spinner";
+import { cn } from "@/lib/utils";
 
 export default function SubmitButton({
   children,
   isSubmitting,
+  variant = "default",
+  className = "",
+  size = "lg",
+  loadingText = "Зачекайте...",
 }: Readonly<{
   children: React.ReactNode;
   isSubmitting: boolean;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | null
+    | undefined;
+  className?: string;
+  size?: "default" | "lg" | "sm" | "icon" | null | undefined;
+  loadingText?: string;
 }>) {
   if (isSubmitting) {
     return (
-      <Button disabled className="w-full" size="lg" type="submit">
+      <Button
+        disabled
+        className={cn("w-full", className)}
+        size={size}
+        type="submit"
+      >
         <Spinner className="text-white" size="small" />
-        Зачекайте...
+        {loadingText}
       </Button>
     );
   }
   return (
-    <Button className="w-full" size="lg" type="submit">
+    <Button
+      variant={variant}
+      className={cn("w-full", className)}
+      size={size}
+      type="submit"
+    >
       {children}
     </Button>
   );
